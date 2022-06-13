6f11269ffbccac65f4f363152c74bd364241af1e
Frank Kwabena Abrokwa <76174290+Kwabena-Abrokwa@users.noreply.github.com>
2022-06-13 17:40:15 +0000

    Capitilize the first

--- capitalizeFirstLetterAlgorithm/index.js
+++ capitalizeFirstLetterAlgorithm/index.js
@@ -1,16 +1,19 @@
-//Capitalize First Letter Algorithm simply capilize the first letter of every word in a sentence that is passed to it
+//Capitalize First Letter Algorithm simply capilize the 
+//first letter of every word in a sentence that is passed 
+//to it
 
 const capitalizeFirstLetterAlgorithm = (sentence) => {
 	let word = sentence.split(" ");
 	let results = "";
 
 	for (let i = 0, n = word.length; i < n; i++) {
-		for (let j = 0, n = word[i].length; j < n; j++) {
-                results = results + word[i][j].toUpperCase()
-		}
+			let char = word[i].substring(0, 1).toUpperCase() + word[i].substring(1);
+			results += char + " ";
 	}
-    let final = results.join(" ")
-    console.log(final);
+
+	console.log(results);
 };
 
-capitalizeFirstLetterAlgorithm("this is Frank Jes");
+capitalizeFirstLetterAlgorithm("follow me on twitter @ kwabena_abrokwa");
+//results = "Follow Me On Twitter @ Kwabena_abrokwa "
+
